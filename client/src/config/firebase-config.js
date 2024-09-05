0eecba58803b405899bfe3fb5437a5c35076428b
xXTitouanXx <17titou@gmail.com>
2024-09-05 17:46:32 +0200

    [ADD] forget some rules

---
+++ client/src/config/firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBhk6P8KuOeaKNP-B5OdoaEbGGlHaXu0as",
+    authDomain: "portfolioabbatetitouan.firebaseapp.com",
+    projectId: "portfolioabbatetitouan",
+    storageBucket: "portfolioabbatetitouan.appspot.com",
+    messagingSenderId: "609500609734",
+    appId: "1:609500609734:web:7384813421d23eec41ebc7"
+};
+
+const app = initializeApp(firebaseConfig);
