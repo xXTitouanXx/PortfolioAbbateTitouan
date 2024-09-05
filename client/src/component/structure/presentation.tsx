d53de5fb3ceabd9793531d62e7c8c67d34d09191
xXTitouanXx <17titou@gmail.com>
2024-09-05 15:47:35 +0200

    [ADD] all gifs except android project and presentation.tsx update

--- client/src/component/structure/presentation.tsx
+++ client/src/component/structure/presentation.tsx
@@ -3,6 +3,13 @@ import {Container} from "@/component/container";
 import Image from "next/image";
 
 export const Presentation = () => {
+    const birthDate = new Date('2002-02-17'); // Date de naissance
+    const today = new Date(); // Date actuelle
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDifference = today.getMonth() - birthDate.getMonth();
+    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
     return (
         <div className="">
             <Container className="flex flex-col items-center justify-center pt-16">
@@ -12,27 +19,21 @@ export const Presentation = () => {
                 <div className="flex flex-col md:flex-row w-full justify-center items-center">
                     <div className="p-4 space-y-4 flex-1">
                         <Typography variant="caption1" theme="Mauve" className="max-w-xl">
-                            Hi! My name is Angel Uriot, I’m 23 and I develop things on my free time. I like mixing
-                            science and math with programming and I’m always looking for new challenges. I made
-                            simulations, artificial intelligence, websites, games and more... (see the project part of
-                            the site).
+                            I'm Abbate Titouan, a dedicated {age}-year-old developer driven by a passion for technology
+                            and innovation. My love for the intersection of science, math, and programming fuels my
+                            desire to create impactful solutions.
                         </Typography>
                         <Typography variant="caption1" theme="Mauve" className="max-w-xl">
-                            In 2018, I entered in the Polytech Paris-Saclay French engineering school in IT major to
-                            learn more about math, science and software development. I discovered a lot of technologies
-                            and theoretical concepts, but I also put my knowledge into practice in various projects and
-                            internships, one of which took place in Singapore and greatly expanded my cultural and
-                            technical horizons.
+                            Throughout my academic journey, I've honed my technical skills at Polytech Montpellier,
+                            where I deepened my understanding of mathematics, physics, chemistry, and computer science.
+                            This solid foundation has enabled me to successfully tackle a wide range of projects, from
+                            developing artificial intelligence to designing intricate websites and engaging games.
                         </Typography>
                         <Typography variant="caption1" theme="Mauve" className="max-w-xl">
-                            I’m now a software engineer at BlaBlaCar, the most widely used car-sharing application in
-                            the world. I work on the backend of BlaBlaCar Daily, a secondary application targeting
-                            short-distance carpooling.
-                        </Typography>
-                        <Typography variant="caption1" theme="Mauve" className="max-w-xl">
-                            Besides all this, I'm a content creator on the Internet where I simplify scientific and
-                            computer science concepts across various social media platforms like YouTube under the
-                            pseudonym Dimension.
+                            My practical experience, gained through internships and personal projects, has equipped me
+                            with the ability to bridge the gap between theoretical knowledge and real-world application.
+                            I'm excited to continue exploring new technologies and contributing to cutting-edge
+                            projects.
                         </Typography>
                     </div>
                     <div className="p-4 flex items-start justify-center flex-1">
