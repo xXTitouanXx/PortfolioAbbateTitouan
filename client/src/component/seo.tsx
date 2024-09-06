06fdd15c4767d10fc78476c49f4c5ec5da2b4d1a
xXTitouanXx <17titou@gmail.com>
2024-09-06 08:07:42 +0200

    [ADD] change seo

--- client/src/component/seo.tsx
+++ client/src/component/seo.tsx
@@ -8,13 +8,10 @@ interface Props {
 export const Seo = ({title, description}: Props) => {
     return (
         <Head>
-            <title>{title}</title>
+            <title>{"Abbate Titouan"}</title>
             <meta name="description" content={description}/>
-            <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1.0"
-            />
-            <link rel="icon" href="/favicon.ico"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            <link rel="icon" href="assets/images/portefeuille.png"/>
         </Head>
     )
 }
