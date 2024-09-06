701a53e3cd2202f40da8158685cba1a93dc8b456
xXTitouanXx <17titou@gmail.com>
2024-09-06 14:21:41 +0200

    [ADD] coding style

--- client/src/component/seo.tsx
+++ client/src/component/seo.tsx
@@ -8,7 +8,7 @@ interface Props {
 export const Seo = ({title, description}: Props) => {
     return (
         <Head>
-            <title>{"Abbate Titouan"}</title>
+            <title>{"Abbate Titouan Portfolio"}</title>
             <meta name="description" content={description}/>
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
             <link rel="icon" href="assets/images/portefeuille.png"/>
